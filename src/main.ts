4c4b6bc1aeff643422772bcc871358e0eeb667b9
Abdulboriy <abdulborimahamatjanov86@gmail.com>
2025-04-06 01:31:50 +0600

    main.ts file has added to src folder

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle(
+      'This is project that named Products with Categories with NestJs and Swagger',
+    )
+    .setDescription(
+      'The Products with Categories, and Authorization, Authentification Rest API description',
+    )
+    .setVersion('1.0')
+    .addSecurityRequirements('bearer', ['bearer'])
+    .addBearerAuth()
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, documentFactory);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  await app.listen(process.env.PORT ?? 3000, () =>
+    console.log(`Server is successfully started on port: ${process.env.PORT}`),
+  );
 }
+
 bootstrap();
