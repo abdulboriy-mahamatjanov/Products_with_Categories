7e49f14224b362978d22d838e0fd60470ff746f7
Abdulboriy <abdulborimahamatjanov86@gmail.com>
2025-04-07 03:29:07 +0600

    app.module file has changed and done successfully

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,39 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { RegionModule } from './region/region.module';
 import { ConfigModule } from '@nestjs/config';
+import { UploadImageModule } from './upload-image/upload-image.module';
+import { CloudinaryModule } from 'nestjs-cloudinary';
+import { AuthModule } from './auth/auth.module';
+import { MailModule } from './mail/mail.module';
+import { JwtModule } from '@nestjs/jwt';
+import { UserModule } from './user/user.module';
+import { ProductsModule } from './products/products.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [PrismaModule, RegionModule, ConfigModule.forRoot()],
+  imports: [
+    PrismaModule,
+    RegionModule,
+    ConfigModule.forRoot(),
+    UploadImageModule,
+    CloudinaryModule.forRootAsync({
+      useFactory: () => ({
+        cloud_name: process.env.CLOUD_NAME,
+        api_key: process.env.API_KEY,
+        api_secret: process.env.API_SECRET,
+      }),
+    }),
+    AuthModule,
+    MailModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET_KEY,
+      signOptions: { expiresIn: '50m' },
+    }),
+    UserModule,
+    ProductsModule,
+    CategoriesModule,
+  ],
   controllers: [],
   providers: [],
 })
