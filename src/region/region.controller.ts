6fb2200d83dd9323eec626e091afe06d09667c5a
Abdulboriy <abdulborimahamatjanov86@gmail.com>
2025-04-07 03:27:30 +0600

    region full crud has finished

--- src/region/region.controller.ts
+++ src/region/region.controller.ts
@@ -1,32 +1,46 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { RegionService } from './region.service';
 import { CreateRegionDto } from './dto/create-region.dto';
 import { UpdateRegionDto } from './dto/update-region.dto';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('region')
 export class RegionController {
   constructor(private readonly regionService: RegionService) {}
 
+  @ApiOperation({ summary: 'Create a new Regions' })
   @Post()
   create(@Body() createRegionDto: CreateRegionDto) {
     return this.regionService.create(createRegionDto);
   }
 
+  @ApiOperation({ summary: 'Get All Regions' })
   @Get()
   findAll() {
     return this.regionService.findAll();
   }
 
+  @ApiOperation({ summary: 'Get One Region with ID' })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.regionService.findOne(id);
   }
 
+  @ApiOperation({ summary: 'Update Regions with ID' })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateRegionDto: UpdateRegionDto) {
     return this.regionService.update(id, updateRegionDto);
   }
 
+  @ApiOperation({ summary: 'Delete Regions with ID' })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.regionService.remove(id);
